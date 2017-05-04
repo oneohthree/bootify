0ccb1dfc3ce1014645eb515ac208d49a519a5a3e
oneohthree <oneohthree@users.noreply.github.com>
2017-05-04 14:46:05 -0400

    Rearrange code

--- bootify.sh
+++ bootify.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#
-# bootify: make bootable USB drives with Windows 7/8/10 installation files
+## bootify: make bootable USB drives with Windows 7/8/8.1/10 installation files
 #
 # Copyright (C) 2015-2017 oneohthree
 #
@@ -18,51 +17,105 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-VER="0.2.2"
+VER="0.2.3"
 
-function usage()
+usage()
 {
-cat <<EOF
-Usage: $0 -d [DEVICE] -s [boot|uefi] -i [ISO]
+	cat <<-EOF
+	Usage: bootify -d [DEVICE] -s [boot|uefi] -i [ISO]
 
-Options:
-
--d [DEVICE]     USB device name, eg /dev/sdb
--s [boot|uefi]  boot method, 'boot' or 'uefi'
--i [ISO FILE]   path to the ISO file
--h              display this help and exit
--l              display available USB devices and exit
--v              display version and exit
-EOF
+	-d [DEVICE]       USB device name, eg /dev/sdb
+	-s [boot|uefi]    boot method, 'boot' or 'uefi'
+	-i [ISO FILE]     path to the ISO file
+	-h                display this help and exit
+	-l                display available USB devices and exit
+	-v                display version and exit
+	EOF
 }
 
-function version()
+version()
 {
 	echo "bootify $VER"
 }
 
-function available_devices
+available_devices()
 {
 	lsblk -ndo tran,name,vendor,model,size | grep usb | tr -s " "  " "
 }
 
-function mbr_part()
+checks()
 {
-	BTS="res/bootstrap"
-	parted -s -a optimal $DEV mktable msdos mkpart primary ntfs 1 100% set 1 boot
-	mkfs.ntfs -f -L BOOTIFY ${DEV}1
-	dd if=$BTS of=$DEV > /dev/null 2>&1 || { echo "Error injecting bootstrap" 1>&2; exit 1; }
-	copy_files
+	# Check parameters
+	if [[ -z "$DEV" ]] || [[ -z "$SCH" ]] || [[ -z "$ISO" ]]
+	then
+		usage
+		exit 1
+	fi
+
+	# Check if dependencies are met
+	DEP="dd file stat lsblk parted mkfs.ntfs mkfs.vfat 7z"
+	for D in $DEP
+	do
+		type $D > /dev/null 2>&1 || { echo "ERROR: $D is missing" 1>&2; exit 1; }
+	done
+
+	# Check if bootstrap exists
+	if [[ ! -f mbr.bin && "$SCH" == "boot" ]]
+	then
+		echo "ERROR: bootstrap file does not exist" 1>&2
+		exit 1
+	fi
+
+	# Check if bootify runs as root
+	if [[ "$EUID" -ne 0 ]]
+	then
+		echo "ERROR: bootify must be run as root" 1>&2
+		exit 1
+	fi
+
+	# Check if $DEV is a block device
+	if [[ ! -b "$DEV" ]]
+	then
+		echo "ERROR: $DEV is not a block device" 1>&2
+		exit 1
+	fi
+
+	# Check if $DEV is a USB device
+	if [[ -z $(lsblk -ndo tran $DEV | grep usb) ]]
+	then
+		echo "ERROR: $DEV is not a USB device" 1>&2
+		exit 1
+	fi
+
+	# Check if $DEV is mounted
+	if [[ ! -z $(grep $DEV /proc/mounts) ]]
+	then
+		echo "ERROR: $DEV is mounted, unmount it and run bootify again" 1>&2
+		exit 1
+	fi
+
+	# Check if $ISO exists and is bootable
+
+	if [[ ! -f "$ISO" ]]
+	then
+		echo "ERROR: $ISO does not exist" 1>&2
+		exit 1
+	elif [[ -z $(file "$ISO" | grep "bootable") ]]
+	then
+		echo "ERROR: $ISO is not bootable" 1>&2
+		exit 1
+	fi
+
+	# Check $DEV capacity
+
+	if [[ $(stat -c %s $ISO) -gt $(lsblk -ndbo size $DEV) ]]
+	then
+		echo "ERROR: The device capacity is insufficient" 1>&2
+		exit 1
+	fi
 }
 
-function gpt_part()
-{   
-	parted -s -a optimal $DEV mktable gpt mkpart primary fat32 1 100%
-	mkfs.vfat -F32 -n BOOTIFY ${DEV}1
-	copy_files
-}
-
-function confirm()
+confirm()
 {
 	DSC=$(lsblk -ndo vendor,model,size $DEV | tr -s " " " ")
 	read -p "$DSC is going to be formated. Do you want to continue? (Y/N)" YN
@@ -78,34 +131,53 @@ function confirm()
 	fi
 }
 
-function copy_files()
+partitioning()
 {
-	mount ${DEV}1 /mnt
-	#mount -o ro $ISO /media
-	#cp -rv /media/* /mnt
-	7z x $(realpath $ISO) -o/mnt
+	if [[ "$SCH" == "boot" ]]
+	then
+		parted -s -a optimal $DEV mktable msdos mkpart primary ntfs 1 100% set 1 boot
+		mkfs.ntfs -f -L BOOTIFY ${DEV}1
+		# dd if=/usr/lib/syslinux/bios/mbr.bin of=$DEV bs=446 count=1 conv=notrunc
+		dd if=mbr.bin of=$DEV bs=446 count=1 conv=notrunc > /dev/null 2>&1 || { echo "Error injecting bootstrap" 1>&2; exit 1; }
+	elif [[ "$SCH" == "uefi" ]]
+	then
+		parted -s -a optimal $DEV mktable gpt mkpart primary fat32 1 100%
+		mkfs.vfat -F32 -n BOOTIFY ${DEV}1
+	else
+		echo "ERROR: Incorrect boot method, use 'boot' or 'uefi'" 1>&2
+		usage
+		exit 1
+	fi
+}
+
+copy_files()
+{
+	mkdir /tmp/{src,dst}
+	mount -t iso9660 -o loop $ISO /tmp/src
+	mount ${DEV}1 /tmp/dst
+	cp -rv /tmp/src/* /tmp/dst
 
 	# Windows 7 missing UEFI boot file workaround
-	# This does not happen with Windows 8/10 installation media
 
 	if [[ "$SCH" == "uefi" ]]
 	then
-		if [[ ! -d /mnt/efi/boot ]]
+		if [[ ! -d /mnt/dst/efi/boot ]]
 		then
-			WIM="/media/sources/install.wim"
+			WIM="/tmp/src/sources/install.wim"
 			EFI="1/Windows/Boot/EFI/bootmgfw.efi"
-			DST="/mnt/efi/boot"		
+			DST="/mnt/dst/efi/boot"		
 			7z e $WIM $EFI -o${DST} > /dev/null 2>&1 || { echo 1>&2 "Error extracting bootmgfw.efi"; exit 1; }
 			mv ${DST}/bootmgfw.efi ${DST}/bootx64.efi
 		fi
 	fi
 
-	#sync
-	#sleep 5
-	#umount /mnt /media
-	umount /mnt
+	sync
+}
+
+finish()
+{
+	umount /tmp/{src,dst}
 	echo "Your USB drive has been bootified!"
-	exit 0
 }
 
 while getopts ":hlvd:s:i:" OPT
@@ -145,111 +217,8 @@ do
 	esac
 done
 
-# bootify needs all the parameters
-
-if [[ -z "$DEV" ]] || [[ -z "$SCH" ]] || [[ -z "$ISO" ]]
-then
-	usage
-	exit 1
-fi
-
-# Check if dependencies are met
-
-DEP="dd file lsblk mkfs.ntfs mkfs.vfat parted sha1sum stat 7z"
-for D in $DEP
-do
-    type $D > /dev/null 2>&1 || { echo "$D is not ready" 1>&2; exit 1; }
-done
-
-# Check if bootstrap exists
-
-SHA=$(sha1sum res/bootstrap | cut -f1 -d " ")
-
-if [[ ! -f res/bootstrap ]]
-then
-	echo "bootstrap does not exist" 1>&2
-	exit 1
-fi
-
-# Check bootstrap integrity
-
-if [[ "$SHA" != "9db0cc63f8b6fda3b50ab82e27625c69026a4735" ]]
-then
-	echo "bootstrap file is corrupt" 1>&2
-	exit 1
-fi
-
-# Make sure bootify runs as root
-
-if [[ "$EUID" -ne 0 ]]
-then
-	echo "bootify must be run as root" 1>&2
-	exit 1
-fi
-
-# Check if $DEV is a block device
-
-if [[ ! -b "$DEV" ]]
-then
-	echo "$DEV is not a block device" 1>&2
-	exit 1
-fi
-
-# Check if $DEV is a USB device
-
-if [[ -z $(lsblk -ndo tran $DEV | grep usb) ]]
-then
-	echo "$DEV is not a USB device" 1>&2
-	exit 1
-fi
-
-# Check if $DEV is mounted
-
-if [[ ! -z $(grep $DEV /proc/mounts) ]]
-then
-	echo "$DEV is mounted, unmount it and run bootify again" 1>&2
-	exit 1
-fi
-
-# Check if $ISO exists and is bootable
-
-if [[ ! -f "$ISO" ]]
-then
-	echo "$ISO does not exist" 1>&2
-	exit 1
-elif [[ -z $(file "$ISO" | grep "bootable") ]]
-then
-	echo "$ISO is not bootable, you have to use a bootable ISO file" 1>&2
-	exit 1
-fi
-
-# Check $DEV capacity
-
-if [[ $(stat -c %s $ISO) -gt $(lsblk -ndbo size $DEV) ]]
-then
-	echo "The device capacity is insufficient" 1>&2
-	exit 1
-fi
-
-# Check if /media directory exists
-
-if [[ ! -d /media ]]
-then
-	mkdir /media
-fi
-
-# Go to partitioning stage based on chosen boot method
-
-if [[ "$SCH" == "boot" ]]
-then
-	confirm
-	mbr_part
-elif [[ "$SCH" == "uefi" ]]
-then
-	confirm
-	gpt_part
-else
-	echo "Incorrect boot method, use 'boot' or 'uefi'" 1>&2
-	usage
-	exit 1
-fi
+checks
+confirm
+partitioning
+copy_files
+finish
